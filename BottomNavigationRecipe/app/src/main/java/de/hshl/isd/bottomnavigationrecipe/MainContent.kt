80f4780e27acefb58c3590e4b7a9e4e1de83a686
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-04-22 17:11:28 +0200

    New recipe Bottom Navigation

--- BottomNavigationRecipe/app/src/main/java/de/hshl/isd/bottomnavigationrecipe/MainContent.kt
+++ BottomNavigationRecipe/app/src/main/java/de/hshl/isd/bottomnavigationrecipe/MainContent.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.KEY_ROUTE
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigate
@@ -13,10 +14,6 @@ import de.hshl.isd.bottomnavigationrecipe.ui.theme.BottomNavigationRecipeTheme
 @Composable
 fun MainContent() {
     val navController = rememberNavController()
-    val items = listOf(
-        Screen.Home,
-        Screen.Dashboard,
-    )
 
     BottomNavigationRecipeTheme {
         Scaffold(
@@ -25,33 +22,42 @@ fun MainContent() {
                     Text(text = "Bottom Navigation")
                 })
             },
-            bottomBar = {
-                BottomNavigation {
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentRoute =
-                        navBackStackEntry?.arguments?.getString(KEY_ROUTE)
-                    items.forEach { screen ->
-                        BottomNavigationItem(
-                            icon = {
-                                Icon(
-                                    imageVector = screen.icon!!,
-                                    contentDescription = stringResource(id = screen.resourceId!!)
-                                )
-                            },
-                            label = { Text(stringResource(screen.resourceId!!)) },
-                            selected = currentRoute == screen.route,
-                            onClick = {
-                                navController.navigate(screen.route) {
-                                    popUpTo = navController.graph.startDestination
-                                    launchSingleTop = true
-                                }
-                            }
-                        )
-                    }
-                }
-            }
+            bottomBar = { BottomBar(navController) }
         ) {
             NavigationHost(navController)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val items = listOf(
+        Screen.Home,
+        Screen.Dashboard,
+    )
+
+    BottomNavigation {
+        val navBackStackEntry by
+        navController.currentBackStackEntryAsState()
+        val currentRoute =
+            navBackStackEntry?.arguments?.getString(KEY_ROUTE)
+        items.forEach { screen ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = screen.icon!!,
+                        contentDescription = stringResource(screen.resourceId!!)
+                    )
+                },
+                label = { Text(stringResource(screen.resourceId!!)) },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo = navController.graph.startDestination
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
     }
 }
