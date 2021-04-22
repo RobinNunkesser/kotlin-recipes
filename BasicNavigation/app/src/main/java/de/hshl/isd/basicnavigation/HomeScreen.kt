28198b19ce54319d5709810480874069e6c35160
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-04-22 14:03:10 +0200

    Refactoring

--- BasicNavigation/app/src/main/java/de/hshl/isd/basicnavigation/HomeScreen.kt
+++ BasicNavigation/app/src/main/java/de/hshl/isd/basicnavigation/HomeScreen.kt
@@ -1,21 +1,15 @@
 package de.hshl.isd.basicnavigation
 
 import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
-import de.hshl.isd.basicnavigation.ui.theme.BasicNavigationTheme
 
 @Composable
 fun Home(navController: NavController) {
-    BasicNavigationTheme {
-        Scaffold {
             Button(onClick = { navController.navigate(Screen.Detail.route) }) {
                 Text(text = "Navigate deeper")
             }
-        }
-    }
 }
 
--- BasicNavigation/app/src/main/java/de/hshl/isd/basicnavigation/MainActivity.kt
+++ BasicNavigation/app/src/main/java/de/hshl/isd/basicnavigation/MainActivity.kt
@@ -3,8 +3,12 @@ package de.hshl.isd.basicnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
+import de.hshl.isd.basicnavigation.ui.theme.BasicNavigationTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -19,5 +23,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainContent() {
     val navController = rememberNavController()
-    NavigationHost(navController)
+
+    BasicNavigationTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar {
+                    Text(text = "Basic Navigation")
+                }
+            }
+        ) {
+            NavigationHost(navController)
+        }
+    }
 }
