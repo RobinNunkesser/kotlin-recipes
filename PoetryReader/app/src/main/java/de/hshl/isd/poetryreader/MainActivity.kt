4660f5a52f4d05e0262c67b633c21e72aa979149
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-10-06 10:13:51 +0200

    UI

--- PoetryReader/app/src/main/java/de/hshl/isd/poetryreader/MainActivity.kt
+++ PoetryReader/app/src/main/java/de/hshl/isd/poetryreader/MainActivity.kt
@@ -1,18 +1,16 @@
 package de.hshl.isd.poetryreader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
+import de.hshl.isd.poetryreader.domain.PoetryReader
 import de.hshl.isd.poetryreader.ui.theme.PoetryReaderTheme
-import org.json.JSONArray
-import org.json.JSONObject
-import org.json.JSONTokener
-import java.io.BufferedReader
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +19,7 @@ class MainActivity : ComponentActivity() {
             PoetryReaderTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
@@ -29,14 +27,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainContent() {
+    val tag = "MainContent"
+    var resultText by remember { mutableStateOf("") }
+    val poetryReader = PoetryReader()
+    val scope = rememberCoroutineScope()
+
+    fun success(value: String) {
+        resultText = value
+    }
+
+    fun failure(error: Throwable) {
+        Log.e(tag, error.localizedMessage!!)
+    }
+
+    Column {
+        Button(onClick = {
+            scope.launch {
+                kotlin.runCatching {
+                    poetryReader.giveMeSomePoetry()
+                }
+                    .onSuccess(::success)
+                    .onFailure(::failure)
+            }
+        }) {
+            Text("Start")
+        }
+        Text(resultText)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PoetryReaderTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
